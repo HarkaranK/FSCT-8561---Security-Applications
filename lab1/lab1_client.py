74c415b5179325d3c30d8d7c7164e3801b3489b5
HarkaranK <98972110+HarkaranK@users.noreply.github.com>
2026-01-17 18:06:09 -0800

    Added comments to client.py

--- lab1/lab1_client.py
+++ lab1/lab1_client.py
@@ -6,7 +6,8 @@ def run_test_client():
     password = "HELLO"
     username = "Harkaran"
 
-    try:
+    try: 
+        # Connects to the server and sends the password to automatically be authorized
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             print(f"Connecting to server at {server_ip}:{server_port}...")
             client_socket.connect((server_ip, server_port))
@@ -15,16 +16,19 @@ def run_test_client():
             response = client_socket.recv(1024).decode()
             print(f"Server response to password: {response.strip()}")
 
+            # Establishes a username for server to use
             if "What is your name?" in response:
                 client_socket.sendall(username.encode())
                 welcome = client_socket.recv(1024).decode()
                 print(f"Server Welcome: {welcome.strip()}")
 
+            # Testing out changing username command
             print("\n Testing Command: HELLO| \n")
             client_socket.sendall("HELLO|Harkaran New Name".encode())
             print(f"Server response: {client_socket.recv(1024).decode().strip()}")
             client_socket.recv(1024) 
 
+            # Testing out the test massage command
             print("\n Testing Command: MSG| \n")
             client_socket.sendall("MSG|Test message, please work".encode())
             print(f"Server response: {client_socket.recv(1024).decode().strip()}")
@@ -34,10 +38,12 @@ def run_test_client():
             client_socket.sendall("EXIT".encode())
             print("Sent EXIT command. Closing connection.")
 
+    # Error handling if server is not reachable or anything else
     except ConnectionRefusedError:
         print("Error: Could not connect to the server")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
+
     run_test_client()
